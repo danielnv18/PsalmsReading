b0209f252135d63a5eda377fd5274b318703b0d6
Daniel Noyola <daniel.noyola.dev@gmail.com>
2025-12-22 18:01:57 -0600

    Refactor ImprecatoryPsalmIds: remove Psalm ID 7 from the exclusion list

--- PsalmsReading.Domain/PsalmRules.cs
+++ PsalmsReading.Domain/PsalmRules.cs
@@ -8,7 +8,7 @@ public static class PsalmRules
     /// Imprecatory psalms are those that invoke curses or divine judgment against enemies.
     /// These psalms are excluded from reading due to their themes of vengeance.
     /// </summary>
-    private static readonly HashSet<int> ImprecatoryPsalmIds = [5, 7, 10, 17, 28, 31, 35, 40, 52, 54, 55, 56, 58, 59, 69, 70, 71, 79, 83, 94, 109, 129, 137, 139, 140, 143];
+    private static readonly HashSet<int> ImprecatoryPsalmIds = [5, 10, 17, 28, 31, 35, 40, 52, 54, 55, 56, 58, 59, 69, 70, 71, 79, 83, 94, 109, 129, 137, 140, 143];
 
     public static bool IsReadable(Entities.Psalm psalm)
     {
